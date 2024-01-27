7ea72e39b113b271098a158e8cb0b667c1daecb9
Vitor Hugo <vitorhugo02cc@gmail.com>
2024-01-27 13:19:40 -0300

    feat: criando o Service de User

---
+++ teste-multipedidos/app/Services/UserService.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\UserRepositoryInterface;
+
+class UserService
+{
+    protected $userRepository;
+
+    public function __construct(UserRepositoryInterface $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    public function createUser(array $data)
+    {
+        return $this->userRepository->create($data);
+    }
+}
