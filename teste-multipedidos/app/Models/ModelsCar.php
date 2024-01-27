e225335d8f96104d11f6c17002ed17c82187027b
Vitor Hugo <vitorhugo02cc@gmail.com>
2024-01-27 11:59:37 -0300

    feat: adicionando model e migration de Cars

---
+++ teste-multipedidos/app/Models/ModelsCar.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Car extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'brand', 'model', 'license_plate', 'color',
+    ];
+
+    protected $dates = ['deleted_at'];
+}
--- teste-multipedidos/database/migrations/2024_01_27_143907_create_cars_table.php
+++ teste-multipedidos/database/migrations/2024_01_27_143907_create_cars_table.php
@@ -6,20 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('cars', function (Blueprint $table) {
             $table->id();
+            $table->string('brand');
+            $table->string('model');
+            $table->string('license_plate');
+            $table->string('color');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('cars');
