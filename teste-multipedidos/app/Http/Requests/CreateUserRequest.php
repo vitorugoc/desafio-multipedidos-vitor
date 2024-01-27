a4c2e3438310f62f8174ea256ea401ba60c2a2f0
Vitor Hugo <vitorhugo02cc@gmail.com>
2024-01-27 14:07:25 -0300

    feat: criando Request de User

---
+++ teste-multipedidos/app/Http/Requests/CreateUserRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateUserRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:6',
+            'birth_date' => 'required|date'
+        ];
+    }
+}
+
