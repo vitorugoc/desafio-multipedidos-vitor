c0db7862098cf06e776e2956243c75b814c1e13b
Vitor Hugo <vitorhugo02cc@gmail.com>
2024-01-27 14:26:06 -0300

    fix: caminho de imports para UserRepository

--- teste-multipedidos/app/Http/Controllers/UserController.php
+++ teste-multipedidos/app/Http/Controllers/UserController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\CreateUserRequest;
-use App\Services\UserService;
+use App\Services\User\UserService;
 use Illuminate\Http\Request;
 use Illuminate\Database\QueryException;
 
