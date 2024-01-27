add995c9a5089ffe06c4d216985139f511ba7d17
Vitor Hugo <vitorhugo02cc@gmail.com>
2024-01-27 15:00:52 -0300

    feat: adicionando mensagens de erro e a sua tratativa

--- teste-multipedidos/app/Http/Controllers/Controller.php
+++ teste-multipedidos/app/Http/Controllers/Controller.php
@@ -9,4 +9,8 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+    protected function formatValidationErrors(\Illuminate\Contracts\Validation\Validator $validator)
+    {
+        return response()->json(['error' => $validator->errors()], 422);
+    }
 }
--- teste-multipedidos/app/Http/Requests/CreateUserRequest.php
+++ teste-multipedidos/app/Http/Requests/CreateUserRequest.php
@@ -20,5 +20,25 @@ class CreateUserRequest extends FormRequest
             'birth_date' => 'required|date'
         ];
     }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'O campo nome é obrigatório.',
+            'name.string' => 'O campo nome deve ser uma string.',
+            'name.max' => 'O campo nome não pode ter mais de :max caracteres.',
+            
+            'email.required' => 'O campo email é obrigatório.',
+            'email.email' => 'O campo email deve ser um endereço de email válido.',
+            'email.unique' => 'Este email já está em uso.',
+            
+            'password.required' => 'O campo senha é obrigatório.',
+            'password.string' => 'O campo senha deve ser uma string.',
+            'password.min' => 'O campo senha deve ter pelo menos :min caracteres.',
+            
+            'birth_date.required' => 'O campo data de nascimento é obrigatório.',
+            'birth_date.date' => 'O campo data de nascimento deve ser uma data válida.'
+        ];
+    }
 }
 
