15d527b35234fed50c02fad81e6f93020d61d801
Vitor Hugo <vitorhugo02cc@gmail.com>
2024-01-27 14:09:50 -0300

    feat: criando o Controller do User

---
+++ teste-multipedidos/app/Http/Controllers/UserController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CreateUserRequest;
+use App\Services\UserService;
+use Illuminate\Http\Request;
+use Illuminate\Database\QueryException;
+
+class UserController extends Controller
+{
+    protected $userService;
+
+    public function __construct(UserService $userService)
+    {
+        $this->userService = $userService;
+    }
+
+    public function createUser(CreateUserRequest $request)
+    {
+        try {
+            $userData = $request->validated();
+
+            $user = $this->userService->createUser($userData);
+
+            return response()->json(['user' => $user], 201);
+        } catch (QueryException $e) {
+            return response()->json(['error' => 'Erro ao criar o usuário.'], 500);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Erro interno do servidor.'], 500);
+        }
+    }
+}
+
