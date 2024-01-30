18e6b42208850f8710e6079492e20ab614251b7a
Vitor Hugo <vitorhugo02cc@gmail.com>
2024-01-30 15:51:44 -0300

    refactor: criando um BaseRepository para abstrair os metodos comuns

---
+++ teste-multipedidos/app/Repositories/Base/BaseRepository.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Exceptions\EntityNotFoundException;
+
+abstract class BaseRepository
+{
+    protected $model;
+
+    public function __construct($model)
+    {
+        $this->model = $model;
+    }
+
+    public function create(array $data)
+    {
+        return $this->model->create($data);
+    }
+
+    public function findById($id)
+    {
+        $entity = $this->model->find($id);
+
+        if (!$entity) {
+            throw new EntityNotFoundException($this->getEntityName());
+        }
+
+        return $entity;
+    }
+
+    public function update($id, $data)
+    {
+        $entity = $this->findById($id);
+        $entity->update($data);
+
+        return $entity;
+    }
+
+    public function delete($id)
+    {
+        $entity = $this->findById($id);
+        $this->handleDeleteRelations($entity);
+
+        return $entity->delete();
+    }
+
+    abstract protected function getEntityName();
+
+    abstract protected function handleDeleteRelations($entity);
+}
